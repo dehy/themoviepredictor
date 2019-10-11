510a2a00b47db646661bfca7099a8c904e11336b
Arnaud de Mouhy <arnaud@admds.net>
2019-10-11 15:23:20 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,83 @@
+import mysql.connector
+import sys
+import argparse
+import csv
+
+def connectToDatabase():
+    return mysql.connector.connect(user='predictor', password='predictor',
+                              host='127.0.0.1',
+                              database='predictor')
+
+def disconnectDatabase(cnx):
+    cnx.close()
+
+def createCursor(cnx):
+    return cnx.cursor(dictionary=True)
+
+def closeCursor(cursor):    
+    cursor.close()
+
+def findQuery(table, id):
+    return ("SELECT * FROM {} WHERE id = {}".format(table, id))
+
+def findAllQuery(table):
+    return ("SELECT * FROM {}".format(table))
+
+def find(table, id):
+    cnx = connectToDatabase()
+    cursor = createCursor(cnx)
+    query = findQuery(table, id)
+    cursor.execute(query)
+    results = cursor.fetchall()
+    closeCursor(cursor)
+    disconnectDatabase(cnx)
+    return results
+
+def findAll(table):
+    cnx = connectToDatabase()
+    cursor = createCursor(cnx)
+    cursor.execute(findAllQuery(table))
+    results = cursor.fetchall()
+    closeCursor(cursor)
+    disconnectDatabase(cnx)
+    return results  
+
+parser = argparse.ArgumentParser(description='Process MoviePredictor data')
+
+parser.add_argument('context', choices=['people', 'movies'], help='Le contexte dans lequel nous allons travailler')
+
+action_subparser = parser.add_subparsers(title='action', dest='action')
+
+list_parser = action_subparser.add_parser('list', help='Liste les entitées du contexte')
+list_parser.add_argument('--export' , help='Chemin du fichier exporté')
+
+find_parser = action_subparser.add_parser('find', help='Trouve une entité selon un paramètre')
+find_parser.add_argument('id' , help='Identifant à rechercher')
+
+args = parser.parse_args()
+
+if args.context == "people":
+    if args.action == "list":
+        people = findAll("people")
+        if args.export:
+            print('exportation')
+            with open(args.export, 'w', encoding='utf-8', newline='\n') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(people[0].keys())
+                for person in people:
+                    writer.writerow(person.values())
+        else:
+            for person in people:
+                print("#{}: {} {}".format(person['id'], person['firstname'], person['lastname']))
+    if args.action == "find":
+        peopleId = args.id
+        people = find("people", peopleId)
+        for person in people:
+            print("#{}: {} {}".format(person['id'], person['firstname'], person['lastname']))
+
+if args.context == "movies":
+    if args.action == "find":  
+        movieId = args.id
+        movies = find("movies", movieId)
+        for movie in movies:
+            print("#{}: {} released on {}".format(movie['id'], movie['title'], movie['release_date']))
